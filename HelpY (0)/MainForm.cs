7d7a9e06f2ea7f29340eab0bcf77cf65c87eca77
Daniil <samuraplay@mail.ru>
2024-08-25 00:15:24 +0300

    First commit

---
+++ HelpY (0)/MainForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HelpY__0_
+{
+    internal class MainForm:Form
+    {
+        LoginForm loginForm;
+        public MainForm():base()
+        {
+            this.WindowState = FormWindowState.Maximized;
+            this.MaximizeBox = false;
+            this.Text = "HelpY";
+            loginForm = new LoginForm();
+            loginForm.ShowDialog();
+        }
+    }
+}
